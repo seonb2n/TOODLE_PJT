604a9bb037fc9e0d3eaf0ca22a72421052f634d6
seonb2n <626ksb@gmail.com>
2021-05-19 10:06:58 +0900

    joinMail Activity 구성

--- app/src/main/java/com/example/toodle/JoinMailActivity.java
+++ app/src/main/java/com/example/toodle/JoinMailActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.function.ToDoubleBiFunction;
 
 public class JoinMailActivity extends BasicActivity {
 
@@ -43,7 +46,86 @@ public class JoinMailActivity extends BasicActivity {
     private void init() {
         setDataLogout();
 
+        mail_EditTxt = findViewById(R.id.mail_EditTxt);
+        mail_EditTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
 
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                setInputResult();
+            }
+        });
+
+        pwRe_EditTxt = findViewById(R.id.pwRe_EditTxt);
+        pwRe_EditTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                setInputResult();
+            }
+        });
+
+        //TODO
+        // EditTxt 에 다음키 누르면 키보드  숨기는 기능 추가.
+
+
+        pwShow_Img = findViewById(R.id.pwShow_Img);
+        pwReShow_Img = findViewById(R.id.pwReShow_Img);
+        userMsg_Txt = findViewById(R.id.userMsg_Txt);
+        joinEmailBtn_Layout = findViewById(R.id.joinEmailBtn_Layout);
+        joinEmailBtn_Img = findViewById(R.id.joinEmailBtn_Img);
 
     }
+
+    public void setInputResult() {
+        // TODO
+        //  만약 mail_EdiText 에 입력이 안되어 있으면, 버튼 비활성화
+        //  입력 되어 있으면 버튼 활성화
+    }
+
+    public void PwShowClicked(View v) {
+        //TODO
+        // 버튼 눌리면 비밀번호 보여줌
+    }
+
+    public void PwReShowClicked(View v) {
+        //TODO
+        //버튼 눌리면 비밀번호 보여줌
+    }
+
+    public void JoinMailClicked(View v) {
+        //TODO
+        //유효성 검사 후 가입 Task 실행
+    }
+
+    @Override
+    public void clearFocusBundle() {
+        mail_EditTxt.clearFocus();
+        pw_EditTxt.clearFocus();
+        pwRe_EditTxt.clearFocus();
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            backActivity();
+        }
+
+        return true;
+    }
+
 }
